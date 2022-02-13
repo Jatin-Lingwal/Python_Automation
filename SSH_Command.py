b3153bd6206b73fe82d282e88b68a7dcbf578e98
Jatin Lingwal <jatinlingwal96@gmail.com>
2022-02-14 00:12:06 +0530

    condition not working properly

---
+++ SSH_Command.py
@@ -0,0 +1,27 @@
+
+# Need to install paramiko module and imported specific function from it.
+from paramiko import SSHClient
+# AutoAddPolicy helps us to add a machine in our known host(if we are connecting with a machine for the first time.)
+from paramiko import AutoAddPolicy
+# To hide password entered by the user
+import getpass
+
+Host_name = input("Enter machine's IP to which you wanna connect to: ")
+User_name = input("Enter username: ")
+Password = getpass.getpass("Enter your password: ")
+Command = input("Enter any command to execute: ")
+def Connection():
+    with SSHClient() as slave:
+# Set policy to use when connecting to servers without a known host key.
+        slave.set_missing_host_key_policy(AutoAddPolicy)
+# If  your using key base authentication key_file=</root/.ssh/id_rsa>
+        slave.connect(hostname=Host_name, username=User_name, password=Password)
+# While using paramiko the output comes in tuple format, it contains 3 values(stdin,stdout,stderr)
+        stdin, stdout, stderr = slave.exec_command(Command)
+        output = stdout.read()
+        if Command == slave.exec_command(Command):
+            print(output.decode())
+        else:
+            print(stderr.read)
+
+Connection()
