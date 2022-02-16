028a69419c689e252319ff442b544b9f9d35d79b
Jatin Lingwal <jatinlingwal96@gmail.com>
2022-02-16 14:13:19 +0530

    ready to play

--- walkthrough_file&dir.py
+++ walkthrough_file&dir.py
@@ -1,21 +1,26 @@
-""""""
-#
+"""Walking through the directories and sub-directories
+for fetching basic information of associated files inside each directory."""
+# os module helps in various operation like os.path
 import os
-# time module will us to know when was the file created
+# time module will help in format of time
 import time
 Main_path = input("Enter path to walk through: ")
 
 
 def walking_with_you(Main_path):
     print(f"Staring walking through the directory {Main_path}: ")
-    sub_dir = os.listdir(Main_Path)
+    sub_dir = os.listdir(Main_path)
 
     for files in sub_dir:
-        files_path = os.path.join(Main_path, files)
+        files_path = os.path.join(Main_path, files) # Connecting Dir with the inside file & dir (to make absolute path)
         if os.path.isfile(files_path):
-            last_access = os.path.getatime(files_path)
-            local_time = time.ctime(last_access)
-            print(local_time)
-            print(files_path)
+            last_access = os.path.getatime(files_path) # when file was created
+            local_time = time.ctime(last_access) # it checks system time format
+            size = os.path.getsize(files_path) # size of file in bytes
+            print(f'File: {files_path}')
+            print(f'\t:Last accessed: {local_time}')
+            print(f'\tSize: {size} Bytes')
+        elif os.path.isdir(files_path):# This condition will take the same above steps if there are more dirs inside the desired path
+            walking_with_you(files_path)# Calling function again to print same infos
 
-
+walking_with_you(Main_path)
